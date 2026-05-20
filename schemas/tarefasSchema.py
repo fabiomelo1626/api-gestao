f7d2c6504146cccf6b758979ffad013fe944aa07
fabiomelo1626 <fabiomelo.1626@gmail.com>
2026-05-20 16:06:49 -0300

    atualização para deploy

--- schemas/tarefasSchema.py
+++ schemas/tarefasSchema.py
@@ -29,19 +29,3 @@ class TarefaCreate(TarefaSchema):
 class TarefaResponse(TarefaSchema):
     id: int
 
-
-class TarefaUpdate(TarefaSchema):
-    user_id : Optional[int] = None
-    local_id : Optional[int] = None
-    data_registro : Optional[datetime] = None
-    data_alteracao : Optional[datetime] = None
-
-    setor_id : Optional[int] = None
-    Nome : Optional[str] = None
-    descricao : Optional[str] = None
-    responsavel : Optional[int] = None
-    meta_id: Optional[int] = None
-    status : Optional[str] = None
-    class Config:
-        from_attributes = True
-
