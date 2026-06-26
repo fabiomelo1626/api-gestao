35768d07491dcf4f57b1eda6328661ab08309d0d
fabiomelo1626 <fabiomelo.1626@gmail.com>
2026-06-26 17:25:34 -0300

    permission user

--- schemas/userPermissionSchema.py
+++ schemas/userPermissionSchema.py
@@ -44,6 +44,7 @@ class PermissionTablesResponse(PermissionTablesSchema):
 class UserPermissionSchema(BaseModel):
     user_id: Optional[int] = None
     local_id: Optional[int] = None
+    permission_table_id: Optional[int] = None
     data_registro: Optional[datetime] = None
     data_alteracao: Optional[datetime] = None
 
