07714b3bcd4189166c354dac0d301337cf3beb50
fabiomelo1626 <fabiomelo.1626@gmail.com>
2026-07-10 18:57:26 -0300

    novas rotas em dashboard

--- endpoints/dashboardEndpoints.py
+++ endpoints/dashboardEndpoints.py
@@ -11,6 +11,7 @@ from models.pessoaModels import Pessoa
 from models.projetosModels import Projeto
 from models.tarefasModels import Tarefa
 from schemas.metasSchema import *
+from schemas.projetosShema import ProjetoResponse
 from utils.middlewareDependence import check_permission
 
 
@@ -35,3 +36,75 @@ def get_tarefas_destaque_all(local_id: int, db: Session = Depends(get_db)):
     atividades =  db.query(Tarefa).filter(Tarefa.local_id==local_id).order_by(Tarefa.data_alteracao.desc()).limit(5).all()
 
     return atividades
+
+
+
+
+
+#CONTAGEM DOS PROJETOS
+@dashboard.get("/projetos-count", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).count()
+
+#todos os atrasados
+@dashboard.get("/projetos-atrasados", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Em atraso").all()
+
+#quantidade de atrasados
+@dashboard.get("/projetos-atrasados-count", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Em atraso").count()
+
+
+
+#todos os concluidos
+@dashboard.get("/projetos-concluidos", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Concluída").all()
+
+#quantidade de concluidos
+@dashboard.get("/projetos-atrasados-count", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Concluída").count()
+
+
+#todos os em andamento
+@dashboard.get("/projetos-em-andamento", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Em andamento").all()
+
+#quantidade de em andamento
+@dashboard.get("/projetos-em-andamento-count", response_model=List[ProjetoResponse])
+def projetos_count_all(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user),
+    
+):
+    return db.query(Projeto).filter(Projeto.status == "Em andamento").count()
+
+
